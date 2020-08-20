af8d4981416d1dc8a28d59915cff870cdb668d5b
Mr_gao <18612792331@163.com>
2020-08-21 07:49:44 +0800

    first

---
+++ src/main/java/com/gaot/spider/processor/PiankuProcessor.java
@@ -0,0 +1,21 @@
+package com.gaot.spider.processor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class PiankuProcessor implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+    @Override
+    public void process(Page page) {
+        
+
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+}
---
+++ src/main/java/com/gaot/spider/resource/IndexResource.java
@@ -0,0 +1,4 @@
+package com.gaot.spider.resource;
+
+public class IndexResource {
+}
